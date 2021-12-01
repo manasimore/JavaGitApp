1d7953b73eee4694eaa450d1409f22e12d68e5d9
manasimore <mansi.more7777@gmail.com>
2021-12-01 14:25:25 +0530

    first commit

---
+++ JavaGitApp/src/main/java/com/lti/demos/ClientApp.java
@@ -0,0 +1,12 @@
+package com.lti.demos;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p1=new Product(101,"Mobile");
+		System.out.println(p1);
+
+	}
+
+}
